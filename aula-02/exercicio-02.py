1400f3c15942af01e536fa2d30451cdaf9cb81ee
Francis Lauriano <130801505+FrancisLauriano@users.noreply.github.com>
2025-08-15 12:10:18 +0000

    aula 02

---
+++ aula-02/exercicio-02.py
@@ -0,0 +1,11 @@
+dividendo = int(input('Informe o dividendo: '))
+divisor = int(input('Informe o divisor: '))
+
+if divisor == 0:
+    print(f'[ERRO] O divisor não pode ser zero!')
+else:    
+    inteiro = dividendo // divisor
+    resto = dividendo % divisor
+
+    print(f'O inteiro da divisão entre ${dividendo} e ${divisor} é: ${inteiro}')
+    print(f'O resto da divisão entre ${dividendo} e ${divisor} é: ${resto}')
---
+++ aula-02/exercicio-03.py
@@ -0,0 +1,16 @@
+# "ax2+bx+c=0"
+
+#  x = (-b ± √Δ) / 2a
+
+valorA = int(input('Inserir o valor de A: '))
+valorB = int(input('Inserir o valor de B: '))
+valorC = int(input('Inserir o valor de C: '))
+
+if valorA == 0:
+    print(f'Não é uma equação do segundo grau')
+else:    
+    delta = (valorB*valorB)-4*valorA*valorC
+
+    if delta < 0:
+        print(f'Não existe raizes reais')
+
