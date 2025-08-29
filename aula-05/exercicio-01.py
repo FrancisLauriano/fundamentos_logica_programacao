ecf535d6e25d364e50a5cde5a9770758a874d342
Francis Lauriano <130801505+FrancisLauriano@users.noreply.github.com>
2025-08-29 12:07:18 +0000

    ok

---
+++ aula-05/exercicio-01.py
@@ -0,0 +1,14 @@
+
+# QUESTÃO 1:
+# a, b, c = map(int, input("Digite três números inteiros: ").split())
+# resultado = (a+b)*c/2
+# print(f"Resultado: {resultado}")
+
+
+# QUESTÃO 2:
+# raio = float(input("Informe o raio: "))
+# PI = 3.14
+# volume = (4/3)* PI *(raio**3)
+# print(f"Resultado: {volume:.2f}")
+
+# QUESTÃO 3:
