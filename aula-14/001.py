2c633ec477b60742674b60e06b6b482b805e2cae
FrancisLauriano <francislauriano@gmail.com>
2025-10-26 19:58:21 -0300

    ok

--- aula-14/001.py
+++ aula-14/001.py
@@ -1,19 +1,53 @@
-n = int(input())
+# n = int(input())
 
-# for i in range(1, n + 1):
-#     for j in range(1, i + 1):
-#         print(j, end = ' ')
-#     print()
-
-# OU
-
-for i in range(1, n + 1):
-    resposta = ''
-    for j in range(1, i + 1):
-        resposta = resposta + str(j) + ' '
-
-    print(resposta)
+# # for i in range(1, n + 1):
+# #     for j in range(1, i + 1):
+# #         print(j, end = ' ')
+# #     print()
 
 # # OU
-# resposta = ''
-# for i  in range(1, n + 1):
+
+# for i in range(1, n + 1):
+#     resposta = ''
+#     for j in range(1, i + 1):
+#         resposta = resposta + str(j) + ' '
+
+#     print(resposta)
+
+# # # OU
+# # resposta = ''
+# # for i  in range(1, n + 1):
+
+
+n = int(input())
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end=' ')
+    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- aula-14/003.py
+++ aula-14/003.py
@@ -1,3 +1,27 @@
+# n1, n2 = map(int, input().split())
+
+# if n1 >= n2:
+#     maior = n1
+#     menor = n2
+# else:
+#     maior = n2
+#     menor = n1
+
+
+
+# for i in range(menor, maior + 1):
+#     soma_divisor = 0
+#     for j in range(1, i):
+#         if i % j == 0:
+#             soma_divisor += j
+
+#     if soma_divisor == i:
+#         print(i)
+#     # if menor % i == 0:
+#     #     soma_divisor += i
+
+
+
 n1, n2 = map(int, input().split())
 
 if n1 >= n2:
@@ -9,13 +33,33 @@ else:
 
 
 
-for i in range(menor, maior + 1):
-    soma_divisor = 0
+for i in range(menor, maior+1):
+    soma = 0
     for j in range(1, i):
         if i % j == 0:
-            soma_divisor += j
+            soma += j
 
-    if soma_divisor == i:
+    if soma == i:
         print(i)
-    # if menor % i == 0:
-    #     soma_divisor += i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
