65a26cbf468da424bd7f60d5b0d6722ca5f8cbb1
Francis Lauriano <130801505+FrancisLauriano@users.noreply.github.com>
2025-11-07 12:10:11 +0000

    ok

---
+++ 001.py
@@ -0,0 +1,42 @@
+# TUPLAS:
+tupla1 = 10, 'joão', 3.2, True
+
+# for - tupla
+for i in tupla1:
+    print(i)
+
+print('------')
+# while - - tupla
+# print(f'Tamanho tupla: {len(tupla1)}')
+i = 0
+while i < len(tupla1):
+    print(tupla1[i])
+    i += 1
+
+print('------')
+
+# LISTAS:
+lista1 = [10, 'joão', 3.2, True]
+
+for i in lista1:
+    print(i)
+
+print('------')
+
+i = 0
+while i < len(lista1):
+    print(lista1[i])
+    i += 1
+
+print('------') 
+
+lista1[1] = 'Fernando'
+print(lista1)
+
+print(lista1[::2])
+
+# for enumerete lista 
+
+# dicionario
+
+
