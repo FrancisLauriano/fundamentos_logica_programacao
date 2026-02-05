842fffe6df10786dc4999e6f58c58597faf52082
FrancisLauriano <francislauriano@gmail.com>
2026-02-04 22:32:45 -0300

    update

--- 03-estrutura-repeticao/extras/007.py
+++ 03-estrutura-repeticao/extras/007.py
@@ -2,17 +2,17 @@ n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
 
 
-menor = n1
+maior = n1
 
-if n2 < menor:
-    menor = n2
+if n2 > maior:
+    maior = n2
     
 produto = 1
 fator = ''
 
 while n1 > 1 and n2 > 1:
     
-    for c in range(2, menor):
+    for c in range(2, maior):
         while n1 % c == 0 or n2 % c == 0:
             if n1 % c == 0 and n2 % c == 0:
                 fator += f'{c}.'
